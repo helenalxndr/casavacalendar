074570b010d5fca1008de7fe8f46ff20f275acdb
helenalxndr <113268167+helenalxndr@users.noreply.github.com>
2026-03-01 05:17:03 +0700

    Update and rename data_loader.py to loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -6,6 +6,6 @@ def load_all():
     model = load_model("model/best_lstm_multikecamatan.h5")
     encoder = joblib.load("model/label_encoder_kecamatan.pkl")
     scaler = joblib.load("model/scaler_rain.pkl")
-    data = pd.read_csv("data/data.csv")
+    data = pd.read_csv("data/data_fix.csv")
     data["index"] = pd.to_datetime(data["index"])
     return model, encoder, scaler, data
