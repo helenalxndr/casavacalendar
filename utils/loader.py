adfd663bb3df847fc4266a136b72bf844ee987d9
helenalxndr <113268167+helenalxndr@users.noreply.github.com>
2026-03-02 06:04:30 +0700

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -2,7 +2,6 @@ import joblib
 import pandas as pd
 from tensorflow.keras.models import load_model
 
-
 def load_all():
     model = load_model("model/best_lstm_multikecamatan.h5", compile=False)
     encoder = joblib.load("model/label_encoder_kecamatan.pkl")
@@ -10,6 +9,9 @@ def load_all():
 
     data = pd.read_csv("data/data_fix.csv")
 
+    # =========================
+    # FIX KOLOM TANGGAL
+    # =========================
     if "tanggal" in data.columns:
         pass
 
@@ -23,6 +25,9 @@ def load_all():
 
     data["tanggal"] = pd.to_datetime(data["tanggal"], errors="coerce")
 
+    # =========================
+    # FIX KOLOM RAIN
+    # =========================
     if "curah_hujan_mm" in data.columns:
         data.rename(columns={"curah_hujan_mm": "rain_mm"}, inplace=True)
 
