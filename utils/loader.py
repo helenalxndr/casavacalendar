2518e2e29cf2740594d800258efc6306953ff0ea
helenalxndr <113268167+helenalxndr@users.noreply.github.com>
2026-03-01 07:53:15 +0700

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -6,6 +6,14 @@ def load_all():
     model = load_model("model/best_lstm_multikecamatan.h5", compile=False)
     encoder = joblib.load("model/label_encoder_kecamatan.pkl")
     scaler = joblib.load("model/scaler_rain.pkl")
+    
     data = pd.read_csv("data/data_fix.csv")
-    data["index"] = pd.to_datetime(data["index"])
+
+    if "index" in data.columns:
+        data.rename(columns={"index": "tanggal"}, inplace=True)
+    elif "date" in data.columns:
+        data.rename(columns={"date": "tanggal"}, inplace=True)
+
+    data["tanggal"] = pd.to_datetime(data["tanggal"])
+
     return model, encoder, scaler, data
