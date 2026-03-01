bfd36a7878c079a68c69977feef04c6de11f01d3
helenalxndr <113268167+helenalxndr@users.noreply.github.com>
2026-03-02 06:01:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,9 +30,9 @@ tanggal_tanam = st.sidebar.date_input("Tanggal Tanam")
 kec_id = encoder.transform([selected_kecamatan])[0]
 
 df_kec = data[data["kecamatan"] == selected_kecamatan].copy()
-df_kec = df_kec.sort_values("index")
+df_kec = df_kec.sort_values("tanggal")
 
-rain_last270 = df_kec["curah_hujan_mm"].values[-270:]
+rain_last270 = df_kec["chirps_power_corrected"].values[-270:]
 
 forecast_30 = recursive_forecast(
     model=model,
