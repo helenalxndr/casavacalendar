f1ffe558dee4e3343250ae2bc871c97877cd55f8
helenalxndr <113268167+helenalxndr@users.noreply.github.com>
2026-04-11 06:34:52 +0700

    Enhance button styling in render_day_button function

--- utils/ui_helpers.py
+++ utils/ui_helpers.py
@@ -1,22 +1,32 @@
 import streamlit as st
 
+import streamlit as st
+
 def render_day_button(col, day, label, color, key, selected=False):
 
     border = "3px solid black" if selected else "1px solid #ddd"
 
-    st.markdown(f"""
+    # CSS hanya untuk BUTTON INI saja (bukan global)
+    html = f"""
     <style>
-    div[data-testid="stButton"] > button {{
-        height: 60px;
+    div[data-testid="stButton"][key="{key}"] > button {{
+        height: 65px;
         border-radius: 10px;
         border: {border};
         background-color: {color} !important;
-        color: white;
-        font-weight: bold;
-        white-space: pre-line;
+        color: white !important;
+        font-weight: 600;
+        padding: 2px;
+        line-height: 1.1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        white-space: normal;
     }}
     </style>
-    """, unsafe_allow_html=True)
+    """
+
+    st.markdown(html, unsafe_allow_html=True)
 
     return col.button(
         f"{day}\n{label}",
