20e0d5ca2cac0aa9f7859ce7a0beeb45bcbcbe28
helenalxndr <113268167+helenalxndr@users.noreply.github.com>
2026-04-11 07:34:10 +0700

    Enhance render_day_button with selected parameter
    Added a 'selected' parameter to customize button border.

--- utils/ui_helpers.py
+++ utils/ui_helpers.py
@@ -1,11 +1,14 @@
 import streamlit as st
 
-def render_day_button(col, day, label, color, key):
+def render_day_button(col, day, label, color, key, selected=False):
+
+    border = "3px solid black" if selected else "1px solid #ddd"
+
     btn_html = f"""
     <div style="
-        height:105px;
+        height:100px;
         border-radius:10px;
-        border:1px solid #e5e7eb;
+        border:{border};
         background-color:{color};
         display:flex;
         flex-direction:column;
@@ -14,13 +17,9 @@ def render_day_button(col, day, label, color, key):
         color:white;
         font-weight:bold;
     ">
-        <div style="font-size:18px;">{day}</div>
-        <div style="font-size:10px;">{label}</div>
+        <div style="font-size:18px">{day}</div>
+        <div style="font-size:10px">{label}</div>
     </div>
     """
 
-    return col.button(
-        btn_html,
-        key=key,
-        use_container_width=True
-    )
+    return col.button(btn_html, key=key, use_container_width=True)
