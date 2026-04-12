5cad1888c1c9ed729e439d4bc72005d15201e2b0
helenalxndr <113268167+helenalxndr@users.noreply.github.com>
2026-04-12 11:02:44 +0700

    Update ui_helpers.py

--- utils/ui_helpers.py
+++ utils/ui_helpers.py
@@ -2,9 +2,12 @@ def render_day_button(col, day, label, color, key, selected=False):
 
     border = "3px solid black" if selected else "1px solid #ddd"
 
+    # unique class per tombol
+    class_name = f"btn_{key}"
+
     col.markdown(f"""
     <style>
-    div[data-testid="stButton"] > button {{
+    .{class_name} button {{
         height: 100px;
         border-radius: 10px;
         border: {border};
@@ -13,18 +16,23 @@ def render_day_button(col, day, label, color, key, selected=False):
         font-weight: bold;
 
         display: flex;
-        flex-direction: column;   /* 🔥 penting */
+        flex-direction: column;
         justify-content: center;
         align-items: center;
 
-        white-space: normal !important; /* 🔥 override */
+        white-space: normal !important;
         text-align: center;
     }}
     </style>
+    <div class="{class_name}">
     """, unsafe_allow_html=True)
 
-    return col.button(
+    clicked = col.button(
         f"{day}\n{label}",
         key=key,
         use_container_width=True
     )
+
+    col.markdown("</div>", unsafe_allow_html=True)
+
+    return clicked
